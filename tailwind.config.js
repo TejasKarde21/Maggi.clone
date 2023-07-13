92851502ac3f94bf05d4f92526cc06137e07b473
Tejas <137605358+TejasKarde21@users.noreply.github.com>
2023-07-14 00:38:54 +0530

    all fiels aree uploded

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {   
+         colors: {
+      "primary": "#000000",
+      10: '#6a3a2a',
+      20: '#492c22',
+      30: '#3dba3b',
+  },},
+  },
+  plugins: [
+     
+  ],
+}
+
